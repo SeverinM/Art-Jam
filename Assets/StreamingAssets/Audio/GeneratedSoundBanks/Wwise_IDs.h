d0be9403fb23e4653bf933f640ad8b874e6dd6d6
LeandreRenouard <leandre.renouard@gmail.com>
2019-03-02 23:38:25 +0100

    Push de wwise

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,88 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_COLOR = 3098905827U;
+        static const AkUniqueID PLAY_COPY = 3231622537U;
+        static const AkUniqueID PLAY_DRONE = 4042141136U;
+        static const AkUniqueID PLAY_GRANDIR = 3832207509U;
+        static const AkUniqueID PLAY_MIC_01 = 2142332559U;
+        static const AkUniqueID PLAY_MIC_02 = 2142332556U;
+        static const AkUniqueID PLAY_MIC_03 = 2142332557U;
+        static const AkUniqueID PLAY_MIC_04 = 2142332554U;
+        static const AkUniqueID PLAY_MORPHING = 2539556056U;
+        static const AkUniqueID STOP_DRONE = 410230462U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace STATE_AMB_INGAME
+        {
+            static const AkUniqueID GROUP = 2630825291U;
+
+            namespace STATE
+            {
+                static const AkUniqueID AMB_INGAME = 993440393U;
+                static const AkUniqueID END_AMBINGAME = 655801746U;
+            } // namespace STATE
+        } // namespace STATE_AMB_INGAME
+
+    } // namespace STATES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID DENSITY_FA_SSGRAIN = 2715217995U;
+        static const AkUniqueID IMMERSION_FA_SSGRAIN = 2481728872U;
+        static const AkUniqueID INSTRUMENT_FA_SSGRAIN = 2317409760U;
+        static const AkUniqueID LOPASS_GRANDIR = 91356267U;
+        static const AkUniqueID PLAYBACK_RATE = 1524500807U;
+        static const AkUniqueID PROXIMITY_FA_SSGRAIN = 1791284502U;
+        static const AkUniqueID RPM = 796049864U;
+        static const AkUniqueID RPM_FA_SSGRAIN = 1656280998U;
+        static const AkUniqueID SIMULATION_FA_SSGRAIN = 2428833394U;
+        static const AkUniqueID SS_AIR_FEAR = 1351367891U;
+        static const AkUniqueID SS_AIR_FREEFALL = 3002758120U;
+        static const AkUniqueID SS_AIR_FURY = 1029930033U;
+        static const AkUniqueID SS_AIR_MONTH = 2648548617U;
+        static const AkUniqueID SS_AIR_PRESENCE = 3847924954U;
+        static const AkUniqueID SS_AIR_RPM = 822163944U;
+        static const AkUniqueID SS_AIR_SIZE = 3074696722U;
+        static const AkUniqueID SS_AIR_STORM = 3715662592U;
+        static const AkUniqueID SS_AIR_TIMEOFDAY = 3203397129U;
+        static const AkUniqueID SS_AIR_TURBULENCE = 4160247818U;
+        static const AkUniqueID VOLUME_GRANDIR = 1873913353U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID SOUNDBANK_PETRI = 4132006485U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MOTION_FACTORY_BUS = 985987111U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID DEFAULT_MOTION_DEVICE = 4230635974U;
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
