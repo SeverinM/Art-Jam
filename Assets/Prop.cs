0e74b138f457a3d56b2f07c0f6a7f87c7de8fe07
Séverin Michaut <severinmichaut@gmail.com>
2019-03-02 02:34:48 +0100

    test

---
+++ Assets/Prop.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prop : MonoBehaviour
+{
+    Mesh mesh;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3[] verts = mesh.vertices;
+        for (int i = 0; i < verts.Length; i++)
+        {
+            verts[i] = verts[i] + (verts[i].normalized * 0.01f);
+        }
+        mesh.vertices = verts;
+    }
+}
